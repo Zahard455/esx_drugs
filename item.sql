277d837ce4197e634f9d3ab3383cff713975bd3b
Zahard455 <animetub3.x@gmail.com>
2023-07-19 01:17:06 +0100

    git commit

---
+++ item.sql
@@ -0,0 +1,9 @@
+INSERT INTO `items` (name, label) VALUES
+  ('plastic_bag','Plastic Bag'),
+  ('weed','Weed'),
+  ('weed_pooch','Weed Bag'),
+  ('opium','Opium'),
+  ('opium_pooch','Opium Bag'),
+  ('coke','Coke'),
+  ('coke_pooch','Coke Bag')
+;
